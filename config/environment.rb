5ab5b224208bf758878802d6ff9606e6fdd5d997
Nathan Menge <nathan.menge@gmail.com>
2013-06-10 22:10:21 -0400

    Added generic yaml file for storing api_keys. Implemented the jw_player api_key

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Yogavideo1::Application.initialize!
+
+# Load all API keys into the API_KEYS
+API_KEYS = YAML.load_file("#{Rails.root}/config/api_keys.yml")
