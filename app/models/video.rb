c5ca4ccfd186643b83e13be1f6d5c24c3ba5ee1f
Nathan Menge <nathan.menge@gmail.com>
2013-06-11 22:37:55 -0400

    Enhanced the index.html.erb to include the video thumbnail.  Removed extraneous stuff.

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,5 @@ class Video < ActiveRecord::Base
   attr_accessible :description, :length_in_seconds, :name, :preview_video, :primary_video, :thumbnail, :uploaded_on
   has_attached_file :preview_video
   has_attached_file :primary_video
-  has_attached_file :thumbnail
+  has_attached_file :thumbnail, :styles => { :small => "150x150>" }
 end
